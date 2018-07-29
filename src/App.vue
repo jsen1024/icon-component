a29f4487e184190d11dec3d2e19de719e4014a60
MCJiang <1219385811@qq.com>
2018-07-29 22:58:57 +0800

    svg-icon

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <span class="svg-container svg-container_login">
+          <svg-icon icon-class="user" />
+    </span>
+    <div class="svg-container">
+       <svg-icon icon-class="password"></svg-icon>
+     </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.svg-container {
+    font-size: 40px;
+    color: red;
+}
+</style>
